5939864c241268c00372028d48da43277dac4073
Ayush <rahulbhartimzp7@gmail.com>
2026-09-14 22:50:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import '@fontsource-variable/inter';
 import '@/theme/global.css';
 import '@/theme/screens.css';
-import '@/theme/tweaks.css';
 import App from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
